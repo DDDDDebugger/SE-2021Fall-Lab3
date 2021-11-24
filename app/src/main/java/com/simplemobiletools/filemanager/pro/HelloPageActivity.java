d412b36761de92bd2d6ca1566054f5f81b96e341
DDDDDebugger <2542071198@qq.com>
2021-11-24 14:47:38 +0800

    perfect the new feature

--- app/src/main/java/com/simplemobiletools/filemanager/pro/HelloPageActivity.java
+++ app/src/main/java/com/simplemobiletools/filemanager/pro/HelloPageActivity.java
@@ -2,7 +2,15 @@ package com.simplemobiletools.filemanager.pro;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 
 public class HelloPageActivity extends AppCompatActivity {
 
@@ -10,5 +18,28 @@ public class HelloPageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hello_page2);
+    }
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        // Do something in response to button
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String name = editText.getText().toString();
+        if(!name.isEmpty()){
+            TextView textView = (TextView) findViewById(R.id.textView);
+
+            long currentTime = System.currentTimeMillis();
+            SimpleDateFormat formatter = new SimpleDateFormat("yyyy.MM.dd HH:mm:ss");
+            Date date = new Date(currentTime);
+            String t = formatter.format(date);
+            name = name.length()>15?name.substring(0, 15):name;
+            textView.setText("Hello, " + name
+                + "\n\nUser: " + System.getProperty("user.name")
+                + "\nOS: " + System.getProperty("os.name")
+                + "\nTime: " + t
+            );
+            textView.setTextColor(Color.WHITE);
+        }
+
     }
 }
